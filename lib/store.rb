7d5d51576af2fefc2909708ab428ec49594f150e
DragonMess <kmylop@hotmail.com>
2020-08-30 15:27:21 +0000

    Validations for both models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
-class Store < ActiveRecord::Base
-has_many :employees
+ class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 2 }
+  validates :annual_revenue, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+  validate :must_have_men_or_women
+  
+  def must_have_men_or_women
+    if !womens_apparel && !mens_apparel
+      errors.add(:womens_apparel, "must be true if men's apparel is false")
+      errors.add(:mens_apparel, "must be true if women's apparel is false")
+    end
+  end
+
 end
